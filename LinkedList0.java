39864d1555960fec5d29a5d78294538512d82b60
Puja Sharma <puja.sh1607@gmail.com>
2026-02-25 21:13:47 +0530

    ll basics

---
+++ LinkedList0.java
@@ -0,0 +1,17 @@
+// basic defining of a linked list
+public class LinkedList0 {
+  public static class Node{
+    int data;
+    Node next;
+    // constructor
+    public Node(int data){
+      this.data=data;
+      this.next=null;
+    }
+  }
+  public static Node head;
+
+  public static void main(String[] args) {
+    LinkedList0 ll=new LinkedList0();
+  }
+}
