6f23aaeea452b9e3c645c55dca9910ede459f052
Puja Sharma <puja.sh1607@gmail.com>
2026-07-01 22:13:23 +0530

    intersection of ll

---
+++ LinkedList10_Intersection.java
@@ -0,0 +1,51 @@
+// Intersection of Two Linked Lists
+// In a system there are two singly linked list. By some programming error, the end node of one
+// of the linked lists got linked to the second list, forming an inverted Y-shaped list. Write a
+// program to get the point where two linked lists merge.
+public class LinkedList10_Intersection {
+  public static class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  // O(m*n) , SC : O(1)
+  public static Node intersection(Node head1, Node head2) {
+    while (head1 != null) {
+      Node temp = head2;
+      while (temp != null) {
+        if (temp == head1) {
+          return head1;
+        }
+        temp = temp.next;
+      }
+      head1 = head1.next;
+    }
+    return null;
+  }
+
+  // O(m+n) , SC : O(1)
+  // two pointers
+  // x=x.next for both heads, until they're equal.... when x->null, point x->head2
+  public static Node intersection2(Node headA, Node headB) {
+    Node x = headA;
+    Node y = headB;
+    while (x != y) {
+      if (x == null) {
+        x = headB;
+      } else {
+        x = x.next;
+      }
+      if (y == null) {
+        y = headA;
+      } else {
+        y = y.next;
+      }
+    }
+    return x;
+  }
+}
